be37d44b7d96b0efe14209653e3982e1d2b719ab
Byron <jamesblocker81@gmail.com>
2018-02-26 11:56:07 -0800

    add studio_params permit for creating new studio

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -13,13 +13,7 @@ class StudiosController < ApplicationController
   end
 
   def create
-    @user = User.find(params[:id])
-    @studio = @user.studios.new
-    @studio.name = params[:studio][:name]
-    @studio.address = params[:studio][:address]
-    @studio.phone = params[:studio][:phone]
-    @studio.website = params[:studio][:website]
-    @studio.image_url = params[:studio][:image_url]
+    @studio = current_user.studios.new(studio_params)
     @studio.save
     redirect_to "/"
   end
@@ -34,5 +28,10 @@ class StudiosController < ApplicationController
     @studio = Studio.find(params[:id])
     @dweed.destroy
     redirect_to "/"
+  end
+  private
+  def studio_params
+    return params.require(:studio).permit(:name, :address, :phone, :website, :image_url)
+
   end
 end
