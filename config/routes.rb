4d9087e2e179a408e63377802696f3e944708333
Byron <jamesblocker81@gmail.com>
2018-02-25 18:22:11 -0800

    conditionally show login/logout buttons globally

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root 'users#index'
   resources :users
+
+  # rails automatic path alias assignment
+  delete '/logout' => 'sessions#destroy', as: :logout
+  resources :sessions, only: [:new, :create]
 end
